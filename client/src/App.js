7f292b7c6d19331dabf5aaa6725f3bdbd06ebb35
mirsahib <mirsahib24@gmail.com>
2020-12-28 19:59:59 +0600

    :heavy_plus_sign: Add bootstrap jquery popper

--- client/src/App.js
+++ client/src/App.js
@@ -1,78 +1,35 @@
-import React, { Component } from 'react';
+import React from "react";
+import "./App.css";
+import Navbar from "./component/Header";
+//import Body from "./component/Body";
 
-import logo from './logo.svg';
-
-import './App.css';
-
-class App extends Component {
-  state = {
-    response: '',
-    post: '',
-    responseToPost: '',
-  };
-
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/api/world', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.post }),
-    });
-    const body = await response.text();
-
-    this.setState({ responseToPost: body });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{this.state.responseToPost}</p>
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+            <div class="input-group mb-3">
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Customer No."
+                aria-label="Customer No."
+                aria-describedby="basic-addon2"
+              />
+              <div class="input-group-append">
+                <button class="btn btn-primary" type="button">
+                  Button
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm">One of three columns</div>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/component/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+function Header() {
+  return (
+    <nav id="nav-prim" className="navbar py-0 navbar-dark bg-dark ">
+      <a className="navbar-brand" href="/#">
+        DPDC Bill Manager
+      </a>
+    </nav>
+  );
+}
+
+export default Header;
