d20fb8090c045971b0ce41d7be8edf8beaa7d73a
mirsahib <mirsahib24@gmail.com>
2020-12-31 16:13:55 +0600

    :goal_net: Catches error when data is not available for given cno :bug: Not fixed  Unhandled Rejection  Error in server controller

--- controller/scraper.controller.js
+++ controller/scraper.controller.js
@@ -1,6 +1,14 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+process.on("unhandledRejection", (reason, promise) => {
+  console.log(
+    "send in the cavalry cause the server is burning (Unhandled Rejection  Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client) "
+  );
+  //reason.stack || reason
+  //code will be updated when i have an error reporting service
+});
+
 async function sleep(millis) {
   return new Promise((resolve) => setTimeout(resolve, millis));
 }
@@ -16,48 +24,55 @@ async function getData(year, month, customer_no, res) {
         .then((response) => {
           //console.log(response.ok);
           const $ = cheerio.load(response.data);
-          const name = $(
+          let name = $(
             "body > div:nth-child(6) > div > div.col-lg-9.col-md-9.col-sm-9 > div > table:nth-child(3) > tbody > tr > td:nth-child(1) > div:nth-child(1)"
-          ).text();
-          const cno = $(
-            "body > div:nth-child(6) > div > div.col-lg-9.col-md-9.col-sm-9 > div > table:nth-child(3) > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(6) > td:nth-child(2)"
-          ).text();
-          const meter_no = $(
-            "body > div:nth-child(6) > div > div.col-lg-9.col-md-9.col-sm-9 > div > table:nth-child(4) > tbody > tr:nth-child(2) > td:nth-child(6)"
-          ).text();
-          const unitRaw = $(
-            "body > div:nth-child(6) > div > div.col-lg-9.col-md-9.col-sm-9 > div > table:nth-child(5) > tbody > tr > td:nth-child(4) > span:nth-child(1)"
-          ).text();
+          );
+          if (name.length) {
+            name = name.text();
+            const cno = $(
+              "body > div:nth-child(6) > div > div.col-lg-9.col-md-9.col-sm-9 > div > table:nth-child(3) > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(6) > td:nth-child(2)"
+            ).text();
+            const meter_no = $(
+              "body > div:nth-child(6) > div > div.col-lg-9.col-md-9.col-sm-9 > div > table:nth-child(4) > tbody > tr:nth-child(2) > td:nth-child(6)"
+            ).text();
+            const unitRaw = $(
+              "body > div:nth-child(6) > div > div.col-lg-9.col-md-9.col-sm-9 > div > table:nth-child(5) > tbody > tr > td:nth-child(4) > span:nth-child(1)"
+            ).text();
 
-          const prevUnit = unitRaw.split("\n")[1].trim();
-          const currUnit = unitRaw.split("\n")[2].trim();
-          const unitConsume = unitRaw.split("\n")[3].trim();
-          const amount = $(
-            "body > div:nth-child(6) > div > div.col-lg-9.col-md-9.col-sm-9 > div > table:nth-child(6) > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(3) > td:nth-child(4)"
-          ).text();
-          const amount_with_fine = $(
-            "body > div:nth-child(6) > div > div.col-lg-9.col-md-9.col-sm-9 > div > table:nth-child(6) > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(4) > td:nth-child(4)"
-          ).text();
-          const data = {
-            id: i + 1,
-            Name: name,
-            Customer_No: cno,
-            Meter_No: meter_no,
-            Previous_Unit: prevUnit,
-            Current_Unit: currUnit,
-            Unit_Consumtion: unitConsume,
-            Amount: Number(amount),
-            Amount_With_Fine: Number(amount_with_fine),
-          };
-          container.push(data);
+            const prevUnit = unitRaw.split("\n")[1].trim();
+            const currUnit = unitRaw.split("\n")[2].trim();
+            const unitConsume = unitRaw.split("\n")[3].trim();
+            const amount = $(
+              "body > div:nth-child(6) > div > div.col-lg-9.col-md-9.col-sm-9 > div > table:nth-child(6) > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(3) > td:nth-child(4)"
+            ).text();
+            const amount_with_fine = $(
+              "body > div:nth-child(6) > div > div.col-lg-9.col-md-9.col-sm-9 > div > table:nth-child(6) > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(4) > td:nth-child(4)"
+            ).text();
+            const data = {
+              id: i + 1,
+              Name: name,
+              Customer_No: cno,
+              Meter_No: meter_no,
+              Previous_Unit: prevUnit,
+              Current_Unit: currUnit,
+              Unit_Consumtion: unitConsume,
+              Amount: Number(amount),
+              Amount_With_Fine: Number(amount_with_fine),
+            };
+            container.push(data);
+          } else {
+            throw $(
+              "body > div:nth-child(6) > div > div.col-lg-9.col-md-9.col-sm-9 > div > h3"
+            ).text();
+          } //end of name check
         }) //end of then
         .catch((err) => {
-          res.status(400).json(err);
+          res.status(400).json({ msg: err });
         }); //end axios
       await sleep(5000); // wait for 5 seconds
     } //end of for loop
   } catch (error) {
-    res.status(400).json(err.toString());
+    res.status(400).json({ msg: error });
   }
   return container;
 }
@@ -71,7 +86,7 @@ const run = async function (req, res) {
     const data = await getData(year, month, customer_no, res);
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json(error.toString());
+    res.status(500).json({ msg: error });
   }
 };
 
